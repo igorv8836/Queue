5a585fa255965e2d33f592ffbee6100d5885d0c3
mac <igorv8836@mail.ru>
2024-02-19 21:59:38 +0300

    fragment_auth, navigation, firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,6 +27,10 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,20 +41,27 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
-    implementation("android.arch.navigation:navigation-fragment:$1.0.0-alpha05")
-    implementation("android.arch.navigation:navigation-ui:$1.0.0-alpha05")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+//    implementation("android.arch.navigation:navigation-fragment:$1.0.0-alpha05")
+//    implementation("android.arch.navigation:navigation-ui:$1.0.0-alpha05")
     androidTestImplementation("android.arch.navigation:navigation-testing:$1.0.0-alpha05")
 
+
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+
+    implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
+    implementation("com.google.firebase:firebase-analytics")
 }
---
+++ app/src/main/java/com/example/queue/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.queue
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
